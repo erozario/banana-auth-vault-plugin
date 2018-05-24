4417f7d602ab769e78217e7b5318a40639f9503b
Eduardo Rozario <eduardoagrj@gmail.com>
2018-05-24 01:17:52 -0300

    Add source code

---
+++ README.md
@@ -0,0 +1,79 @@
+# Banana Auth Vault Plugin
+
+This repository contains sample code for a Hashicorp Vault Auth Plugin based on article <https://www.hashicorp.com/blog/building-a-vault-secure-plugin> 
+
+
+## Testing Implementation:
+
+* Create a temporary directory to compile the plugin into and to use as the plugin directory for Vault:
+
+```shell
+$ mkdir -p /tmp/vault-plugins
+```
+
+* Compile the plugin into the temporary directory:
+
+```shell
+$ go build -o /tmp/vault-plugins/banana-auth-vault-plugin
+```
+
+* Create a configuration file to point Vault at this plugin directory:
+
+```shell
+$ tee /tmp/vault.hcl <<EOF
+plugin_directory = "/tmp/vault-plugins"
+EOF
+```
+
+* Start a Vault server in development mode with the configuration:
+
+```shell
+$ vault server -dev -dev-root-token-id="root" -config=/tmp/vault.hcl
+```
+
+* Leave this running and open a new tab or terminal window. Authenticate to Vault:
+
+```shell
+$ export VAULT_ADDR='http://127.0.0.1:8200'
+$ vault auth root
+```
+
+* Calculate and register the SHA256 sum of the plugin in Vault's plugin catalog:
+
+```shell
+$ SHASUM=$(shasum -a 256 "/tmp/vault-plugins/banana-auth-vault-plugin" | cut -d " " -f1)
+vault write sys/plugins/catalog/banana-auth-vault-plugin \
+  sha_256="$SHASUM" \
+  command="banana-auth-vault-plugin"
+```
+
+* Enable the auth plugin:
+
+```shell
+$ vault auth enable -path=banana -plugin-name=banana-auth-vault-plugin plugin
+```
+
+* At this point, the plugin is registered and enabled. To test the implementation, submit a login request with an invalid secret:
+
+```shell
+$ vault write auth/banana/login password="laalala"
+
+Error writing data to auth/banana/login: Error making API request.
+
+URL: PUT http://127.0.0.1:8200/v1/auth/banana/loginCode: 403. Errors:
+
+* permission denied
+```
+
+* Now submit a login request with the correct shared secret:
+
+```shell
+$vault write auth/banana/login password="banana"  
+
+Key                Value
+---                -----
+token              244b6510-99f2-a2dd-01c2-90a928a191be
+token_accessor     279b6660-ce07-e57e-daaa-86a65d5950a6
+token_duration     30stoken_renewable    true
+token_policies     [default]token_meta_song    lucille
+```
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"crypto/subtle"
+	"errors"
+	"log"
+	"os"
+	"time"
+
+	"github.com/hashicorp/vault/helper/pluginutil"
+	"github.com/hashicorp/vault/logical"
+	"github.com/hashicorp/vault/logical/framework"
+	"github.com/hashicorp/vault/logical/plugin"
+)
+
+func main() {
+	apiClientMeta := &pluginutil.APIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	flags.Parse(os.Args[1:])
+
+	tlsConfig := apiClientMeta.GetTLSConfig()
+	tlsProviderFunc := pluginutil.VaultPluginTLSProvider(tlsConfig)
+
+	if err := plugin.Serve(&plugin.ServeOpts{
+		BackendFactoryFunc: Factory,
+		TLSProviderFunc:    tlsProviderFunc,
+	}); err != nil {
+		log.Fatal(err)
+
+	}
+}
+
+func Factory(ctx context.Context, c *logical.BackendConfig) (logical.Backend, error) {
+	b := Backend(c)
+	if err := b.Setup(ctx, c); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+type backend struct {
+	*framework.Backend
+}
+
+func Backend(c *logical.BackendConfig) *backend {
+	var b backend
+
+	b.Backend = &framework.Backend{
+		BackendType: logical.TypeCredential,
+		AuthRenew:   b.pathAuthRenew,
+		PathsSpecial: &logical.Paths{
+			Unauthenticated: []string{"login"},
+		},
+		Paths: []*framework.Path{
+			&framework.Path{
+				Pattern: "login",
+				Fields: map[string]*framework.FieldSchema{
+					"password": &framework.FieldSchema{
+						Type: framework.TypeString,
+					},
+				},
+				Callbacks: map[logical.Operation]framework.OperationFunc{
+					logical.UpdateOperation: b.pathAuthLogin,
+				},
+			},
+		},
+	}
+
+	return &b
+}
+
+func (b *backend) pathAuthLogin(_ context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+	password := d.Get("password").(string)
+
+	if subtle.ConstantTimeCompare([]byte(password), []byte("banana")) != 1 {
+		return nil, logical.ErrPermissionDenied
+	}
+
+	// Compose the response
+	return &logical.Response{
+		Auth: &logical.Auth{
+			InternalData: map[string]interface{}{
+				"secret_value": "abcd1234",
+			},
+			Policies: []string{},
+			Metadata: map[string]string{
+				"song": "lucille",
+			},
+			LeaseOptions: logical.LeaseOptions{
+				TTL:       30 * time.Second,
+				MaxTTL:    60 * time.Minute,
+				Renewable: true,
+			},
+		},
+	}, nil
+}
+
+func (b *backend) pathAuthRenew(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+	if req.Auth == nil {
+		return nil, errors.New("request auth was nil")
+	}
+
+	secretValue := req.Auth.InternalData["secret_value"].(string)
+	if secretValue != "abcd1234" {
+		return nil, errors.New("internal data does not match")
+	}
+
+	return framework.LeaseExtend(30*time.Second, 60*time.Minute, b.System())(ctx, req, d)
+}
